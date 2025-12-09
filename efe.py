a50c4386d84b35c236ea0c4dc8c9320729f7d1a5
efekaankocer <efekaankocer3@gmail.com>
2025-12-09 16:25:13 +0300

    projeçalışması

---
+++ efe.py
@@ -0,0 +1,25 @@
+print("\033[1;32;40m")
+#print("╔"+"═"*20+"╗")
+print("╔═════════════════════╗")
+print("║\033[1;31;40m   VEKTOREL APP  \033[1;32;40m    ║")
+print("║                     ║")
+print("║  1-Toplama          ║")
+print("║  2-Çıkarma          ║")
+print("║  3-hesaplamala      ║")
+print("║  4-                 ║")
+print("║                     ║")
+print("║    Seçimiz nedir?   ║")
+print("╚═════════════════════╝")
+
+secim = input ()
+print (secim *5 )
+
+if secim == " 1 " :
+     print("çizim mi yapmak istiyorsun")
+
+if secim == " 2 " :
+     print("çizim mi yapmak istiyorsun")
+
+if secim == " 3 " :
+     print("çizim mi yapmak istiyorsun")
+     
